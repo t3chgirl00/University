37353846b4f64effd00382f757cd6c65ab6ca87a
t3chgirl00 <keziah.odoi@turntabl.io>
2021-11-01 17:03:35 +0000

    Work

---
+++ src/Subject.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public enum Subject {
+    ENGLISH, MATHS, PHYSICS, PROGRAMMING, GARDENING, INTROTOPROGRAMMING, ADVANCEDGARDENING, ADVANCEDPHYSICS
+
+    public void createSubject(Lecturer lecturer, List<Student> studentList, Level level) {}
+}
